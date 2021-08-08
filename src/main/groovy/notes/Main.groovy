61450e209dbe4a6f0f2b2fea4f701aed00f0769c
Thornton <thornton@rosesquared.org>
2021-08-07 21:54:24 -0400

    Implement and test --add.

--- src/main/groovy/notes/Main.groovy
+++ src/main/groovy/notes/Main.groovy
@@ -2,7 +2,6 @@ package notes
 
 class Main {
 	public static void main(String[] args) {
-		println args
 		def cli = new CliBuilder(usage: "notes <options> <args>")
 		cli._ longOpt: "help", "show usage"
 		cli._ longOpt: "add", args: 2, argName: "args", "add note (args = <title> <text>)"
@@ -12,16 +11,20 @@ class Main {
 		def opts = cli.parse(args)
 		if (!opts) { return }
 
-		if (opts.options.size() == 0 || opts.help) {
+		if (opts.getOptions().size() == 0 || opts.help) {
 			cli.usage()
 			return
 		}
 
 		Notes.load()
-		opts.adds && Notes.add(*opts.adds)
+		opts.adds && show(Notes.add(*opts.adds))
 		opts.updates && Notes.update(*opts.updates)
 		opts.delete && Notes.delete(*opts.delete)
 		opts.list && println(Notes.list())
 		Notes.save()
+	}
+
+	static show(note) {
+		println "${note.id} ${note.title} ${note.text}"
 	}
 }
